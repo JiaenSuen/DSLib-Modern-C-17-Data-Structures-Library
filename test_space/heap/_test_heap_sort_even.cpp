6f36cee564fd7424164fc3ad018f3ab33fddd06f
JiaenSuen <john20050826@gmail.com>
2025-05-01 17:27:26 +0800

    Change Contain of Heap Test File
    Change Contain of Heap Test File , So show it more clear and useful

--- test_space/heap/_test_heap_sort_even.cpp
+++ test_space/heap/_test_heap_sort_even.cpp
@@ -7,33 +7,13 @@ using namespace std;
 
 
 
-vector<int> generate_arr(int size){
-    random_device RD;
-    mt19937 gen(RD());
-    uniform_int_distribution<int> dist(0,100);
-    vector<int> container;
-    for (int i = 0; i < size; i++) container.push_back(dist(gen)); 
-    return container;
-}
 
-void show_vec(vector<int>& List){
-    for (auto& x : List)
-        cout<<x<<"\t";
-    cout<<endl;
-}
 
-void show_vec_pointer(vector<int*>& List){
-    for (auto& x : List)
-        cout<<*x<<"\t";
-    cout<<endl;
-}
-
-void swap_pointer(int* x , int* y){
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-
+// tool kit
+vector<int> generate_arr(int size,int min , int max);
+void show_vec(vector<int>& List);
+void show_vec_pointer(vector<int*>& List);
+// tool kit
 
 
 vector<int*> GetEven(vector<int>& nums) {
@@ -44,57 +24,80 @@ vector<int*> GetEven(vector<int>& nums) {
     return List;
 }
 
-
-// Heap Sort 
-vector<int> Heap_Sort(vector<int>& Array , bool from_Min=true){
+// Heap Sort  
+void Heap_Sort_for_Pointer(vector<int*>& Array , bool from_Min = true){
+    if (Array.empty()) return;
     unique_ptr<Heap<int>> pool;
     if(from_Min)  pool = make_unique< MinHeap<int> >();
-    else pool = make_unique< MaxHeap<int> >();
-    
-    vector<int> result_list;
-    for (auto x :  Array) pool->insert(x);
-    while (!pool->isEmpty()) result_list.push_back(pool->extract_root());
-    return result_list;
-}
+    else          pool = make_unique< MaxHeap<int> >();
 
-
-void Heap_Sort_for_Pointer(vector<int*>& Array , bool from_Min){
-    unique_ptr<Heap<int>> pool;
-    if(from_Min)  pool = make_unique< MinHeap<int> >();
-    else pool = make_unique< MaxHeap<int> >();
-
-    for (auto x :  Array) pool->insert(*x);
-    int i = 0;
+    for (auto& x :  Array) pool->insert(*x);
+    size_t i = 0;
     while (!pool->isEmpty()) {
         *Array[i] = pool->extract_root();
         i++;
     }
-    
 }
-
-
 
 int main()
 {   
-    
-    
-    vector<int>  random_arr = generate_arr(20);
-    vector<int>  sorted_min,sorted_max;
-    cout<<"Show Vec : ";show_vec(random_arr);    
-    sorted_min = Heap_Sort(random_arr,true );
-    sorted_max = Heap_Sort(random_arr,false);
-    cout<<"From Min : ";show_vec(sorted_min);
-    cout<<"From Max : ";show_vec(sorted_max);
+    vector<int>  random_arr; 
+    vector<int*> even_arr;
+    // min , max , size of list
+    vector<vector<int>> params = { 
+        {0,100,10},
+        {-100,100,10},
+        {0,1000,50},
+    };
 
+    string frame_line(100, '-');
+    for (auto p : params)    {
+        cout<<frame_line<<"\n\n";
 
+        random_arr = generate_arr(p[2],p[0],p[1]);
+        even_arr   = GetEven(random_arr);
+        cout<<"Test Case : "<<endl;
+        show_vec(random_arr);
+        cout<<"Sorted Main Array : "<<endl;
+        Heap_Sort_for_Pointer(even_arr ,true); // 輸入 false 由 大排至小
+        show_vec(random_arr);
+        cout<<"Sorted Sub  Array : "<<endl;
+        show_vec_pointer(even_arr);
 
-    vector<int> Nums = generate_arr(20);
-    vector<int*> Numsp = GetEven(Nums);
-    
-    cout<<"\n\n\n";
-    show_vec(Nums);
-    Heap_Sort_for_Pointer(Numsp,true);
-    show_vec(Nums);
-    show_vec_pointer(Numsp);
+        random_arr.clear();
+        even_arr.clear();
+        cout<<frame_line<<"\n\n";
+    }
+    system("pause");
     return 0;
+}
+
+// Tools :
+
+vector<int> generate_arr(int size,int min=0 , int max=100){
+    random_device RD;
+    mt19937 gen(RD());
+    uniform_int_distribution<int> dist(min,max);
+    vector<int> container;
+    for (int i = 0; i < size; i++) container.push_back(dist(gen)); 
+    return container;
+}
+
+
+void show_vec(vector<int>& List){
+    int c = 0;
+    for (auto& x : List) {
+        cout<<x<<"\t";
+        if ((c++)%10==9) cout<<endl;
+    }
+    cout<<endl;
+}
+
+void show_vec_pointer(vector<int*>& List){
+    int c = 0;
+    for (auto& x : List) {
+        cout<<*x<<"\t";
+        if ((c++)%10==9) cout<<endl;
+    }
+    cout<<endl;
 }
